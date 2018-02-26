f602bb5fac65dc1de4a9a7ed23175f7834aaa08b
Pravin Raj <pravin.raj@coveiot.com>
2018-02-26 18:19:01 +0530

    added light discovery

--- index.js
+++ index.js
@@ -164,13 +164,14 @@ noble.on('stateChange', function(state) {
 
           if(light.reachable && light.on) {
               console.log('Light is reachable and ON')
-              brightness = light.brightness + 10;
-              if(brightness > 254) {
-                  brightness = 10;
-              }
-              console.log('Setting brightness to -> ' + brightness);
-              light.brightness = brightness;
-              light.save();
+            //   brightness = light.brightness + 10;
+            //   if(brightness > 254) {
+            //       brightness = 10;
+            //   }
+            //   console.log('Setting brightness to -> ' + brightness);
+            //   light.brightness = brightness;
+            //   light.save();
+            light.incrementBrightness();
           }
         }
       });
