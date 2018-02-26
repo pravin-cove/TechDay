d44b0db711ed386ab308f9497418e62aa30c3e98
Pravin Raj <pravin.raj@coveiot.com>
2018-02-26 18:22:36 +0530

    added light discovery

--- index.js
+++ index.js
@@ -133,49 +133,41 @@ noble.on('stateChange', function(state) {
             break;
           case 'S2':
             if(hueBridgeClient) {
-                hueBridgeClient.lights.getAll()
-      .then(lights => {
-        for (let light of lights) {
-          console.log(`Light [${light.id}]: ${light.name}`);
-          console.log(`  Type:             ${light.type}`);
-          console.log(`  Unique ID:        ${light.uniqueId}`);
-          console.log(`  Manufacturer:     ${light.manufacturer}`);
-          console.log(`  Model Id:         ${light.modelId}`);
-          console.log('  Model:');
-          console.log(`    Id:             ${light.model.id}`);
-          console.log(`    Manufacturer:   ${light.model.manufacturer}`);
-          console.log(`    Name:           ${light.model.name}`);
-          console.log(`    Type:           ${light.model.type}`);
-          console.log(`    Color Gamut:    ${light.model.colorGamut}`);
-          console.log(`    Friends of Hue: ${light.model.friendsOfHue}`);
-          console.log(`  Software Version: ${light.softwareVersion}`);
-          console.log('  State:');
-          console.log(`    On:         ${light.on}`);
-          console.log(`    Reachable:  ${light.reachable}`);
-          console.log(`    Brightness: ${light.brightness}`);
-          console.log(`    Color mode: ${light.colorMode}`);
-          console.log(`    Hue:        ${light.hue}`);
-          console.log(`    Saturation: ${light.saturation}`);
-          console.log(`    X/Y:        ${light.xy[0]}, ${light.xy[1]}`);
-          console.log(`    Color Temp: ${light.colorTemp}`);
-          console.log(`    Alert:      ${light.alert}`);
-          console.log(`    Effect:     ${light.effect}`);
-          console.log();
-
-          if(light.reachable && light.on) {
-              console.log('Light is reachable and ON')
-            //   brightness = light.brightness + 10;
-            //   if(brightness > 254) {
-            //       brightness = 10;
-            //   }
-            //   console.log('Setting brightness to -> ' + brightness);
-            //   light.brightness = brightness;
-            //   light.save();
-            light.incrementBrightness();
-          }
-        }
-      });
+                hueBridgeClient.groups.getAll()
+  .then(groups => {
+    for (let group of groups) {
+      console.log(`Group [${group.id}]: ${group.name}`);
+      console.log(`  Type: ${group.type}`);
+      console.log(`  Class: ${group.class}`);
+      console.log('  Light Ids: ' + group.lightIds.join(', '));
+      console.log('  State:');
+      console.log(`    Any on:     ${group.anyOn}`);
+      console.log(`    All on:     ${group.allOn}`);
+      console.log('  Action:');
+      console.log(`    On:         ${group.on}`);
+      console.log(`    Brightness: ${group.brightness}`);
+      console.log(`    Color mode: ${group.colorMode}`);
+      console.log(`    Hue:        ${group.hue}`);
+      console.log(`    Saturation: ${group.saturation}`);
+      console.log(`    X/Y:        ${group.xy[0]}, ${group.xy[1]}`);
+      console.log(`    Color Temp: ${group.colorTemp}`);
+      console.log(`    Alert:      ${group.alert}`);
+      console.log(`    Effect:     ${group.effect}`);
+ 
+      if (group.modelId !== undefined) {
+        console.log(`  Model Id: ${group.modelId}`);
+        console.log(`  Unique Id: ${group.uniqueId}`);
+        console.log('  Model:');
+        console.log(`    Id:           ${group.model.id}`);
+        console.log(`    Manufacturer: ${group.model.manufacturer}`);
+        console.log(`    Name:         ${group.model.name}`);
+        console.log(`    Type:         ${group.model.type}`);
+      }
+ 
+      console.log();
     }
+  });
+            }
             break;
           case 'S3':
             console.log('S3 clicked');
