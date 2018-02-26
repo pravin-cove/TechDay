5ce44cafd75876f37f4cb0554094ba7a4c596124
Pravin Raj <pravin.raj@coveiot.com>
2018-02-26 18:26:33 +0530

    added light discovery

--- index.js
+++ index.js
@@ -133,39 +133,17 @@ noble.on('stateChange', function(state) {
             break;
           case 'S2':
             if(hueBridgeClient) {
-                hueBridgeClient.groups.getAll()
-  .then(groups => {
-    for (let group of groups) {
-      console.log(`Group [${group.id}]: ${group.name}`);
-      console.log(`  Type: ${group.type}`);
-      console.log(`  Class: ${group.class}`);
-      console.log('  Light Ids: ' + group.lightIds.join(', '));
-      console.log('  State:');
-      console.log(`    Any on:     ${group.anyOn}`);
-      console.log(`    All on:     ${group.allOn}`);
-      console.log('  Action:');
-      console.log(`    On:         ${group.on}`);
-      console.log(`    Brightness: ${group.brightness}`);
-      console.log(`    Color mode: ${group.colorMode}`);
-      console.log(`    Hue:        ${group.hue}`);
-      console.log(`    Saturation: ${group.saturation}`);
-      console.log(`    X/Y:        ${group.xy[0]}, ${group.xy[1]}`);
-      console.log(`    Color Temp: ${group.colorTemp}`);
-      console.log(`    Alert:      ${group.alert}`);
-      console.log(`    Effect:     ${group.effect}`);
- 
-      if (group.modelId !== undefined) {
-        console.log(`  Model Id: ${group.modelId}`);
-        console.log(`  Unique Id: ${group.uniqueId}`);
-        console.log('  Model:');
-        console.log(`    Id:           ${group.model.id}`);
-        console.log(`    Manufacturer: ${group.model.manufacturer}`);
-        console.log(`    Name:         ${group.model.name}`);
-        console.log(`    Type:         ${group.model.type}`);
-      }
- 
-      console.log();
-    }
+  hueBridgeClient.groups.getById(1)
+  .then(group => {
+    group.incrementBrightness        = 50;
+
+    return hueBridgeClient.groups.save(group);
+  })
+  .then(group => {
+    console.log('New saturation:', group.saturation);
+  })
+  .catch(error => {
+    console.log(error.stack);
   });
             }
             break;
