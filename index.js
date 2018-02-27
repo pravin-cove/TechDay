ec7e5531b92079e1e4097c2777ab785a33d3a949
Pravin Raj <pravin.raj@coveiot.com>
2018-02-27 11:32:23 +0530

    Added to print scenes

--- index.js
+++ index.js
@@ -22,97 +22,97 @@ var hueBridgeClient;
  * Listen to the state change and start scanning for BLE devices
  * when the Bluetooth adapter turns ON.
  */
-noble.on('stateChange', function(state) {
+noble.on('stateChange', function (state) {
     if (state === 'poweredOn') {
         console.log('Scanning for devices...');
         noble.startScanning();
     } else {
         noble.stopScanning();
     }
-  });
+});
 
-  /**
-   * See if we have found all required device and connect to those devices
-   * and stop scanning once all required devices are found.
-   */
-  noble.on('discover', (peripheral) => {
-     // console.log(`Found [${peripheral.advertisement.localName}]`);
-    if(peripheral.id == titanWEMacAddress) {
-      console.log('Titan WE watch discovered.');
-      isTitanWeFound = true;
-      connectToTitanWeWatch(peripheral);
+/**
+ * See if we have found all required device and connect to those devices
+ * and stop scanning once all required devices are found.
+ */
+noble.on('discover', (peripheral) => {
+    // console.log(`Found [${peripheral.advertisement.localName}]`);
+    if (peripheral.id == titanWEMacAddress) {
+        console.log('Titan WE watch discovered.');
+        isTitanWeFound = true;
+        connectToTitanWeWatch(peripheral);
     }
-    if(isTitanWeFound) {
-      noble.stopScanning();
+    if (isTitanWeFound) {
+        noble.stopScanning();
     }
 });
 
-  /**
-   * Start searching for Philips Hue Bridge and if discovered create a client.
-   */
-  console.log('Searching for Hue Bridges in local network...')
-  huejay.discover()
-  .then(bridges => {
-      var bridgeIp;
-    for (let bridge of bridges) {
-      console.log(`Bridge found -> Id: ${bridge.id}, IP: ${bridge.ip}`);
-      bridgeIp = bridge.ip;
-    }
-    hueBridgeClient = new huejay.Client({
-        host:     bridgeIp,
-        username: '5OnfNdyaHCAWjp6fv9LY5Fn6Hi2MoDk8o0gZHyYu',
-        timeout:  15000,            
-      });
-      hueBridgeClient.bridge.ping()
-        .then(() => {
-            console.log('Successful connection');
-            hueBridgeClient.bridge.isAuthenticated()
-                .then(() => {
-                    console.log('Successful authentication');
-                })
-                .catch(error => {
-                    console.log('Could not authenticate');
-                });
-        })
-        .catch(error => {
-            console.log('Could not connect');
+/**
+ * Start searching for Philips Hue Bridge and if discovered create a client.
+ */
+console.log('Searching for Hue Bridges in local network...')
+huejay.discover()
+    .then(bridges => {
+        var bridgeIp;
+        for (let bridge of bridges) {
+            console.log(`Bridge found -> Id: ${bridge.id}, IP: ${bridge.ip}`);
+            bridgeIp = bridge.ip;
+        }
+        hueBridgeClient = new huejay.Client({
+            host: bridgeIp,
+            username: '5OnfNdyaHCAWjp6fv9LY5Fn6Hi2MoDk8o0gZHyYu',
+            timeout: 15000,
         });
+        hueBridgeClient.bridge.ping()
+            .then(() => {
+                console.log('Successful connection');
+                hueBridgeClient.bridge.isAuthenticated()
+                    .then(() => {
+                        console.log('Successful authentication');
+                    })
+                    .catch(error => {
+                        console.log('Could not authenticate');
+                    });
+            })
+            .catch(error => {
+                console.log('Could not connect');
+            });
     })
-  .catch(error => {
-    console.log(`An error occurred: ${error.message}`);
-  });
+    .catch(error => {
+        console.log(`An error occurred: ${error.message}`);
+    });
 
-  /**
-   * Here we try to connect to particualr device and discover
-   * its services.
-   */
-  function connectToTitanWeWatch(titanWeWatch) {
-      titanWeWatch.on('disconnect', () => {
-          console.log('Titan WE watch disconnected.');
-          console.log('Scanning for devices...');
-          noble.startScanning();
-      });
-      titanWeWatch.connect((error) => {
-        if(error) {
+/**
+ * Here we try to connect to particualr device and discover
+ * its services.
+ */
+function connectToTitanWeWatch(titanWeWatch) {
+    titanWeWatch.on('disconnect', () => {
+        console.log('Titan WE watch disconnected.');
+        console.log('Scanning for devices...');
+        noble.startScanning();
+    });
+    titanWeWatch.connect((error) => {
+        if (error) {
             throw error;
             return
         }
         console.log('Connected to Titan We watch.');
         discoverTitanWEServices(titanWeWatch);
     });
-  }
-  /**
-   * See if we can discover required service & chacterstic, and ssubscribe to it.
-   * @param {peripheral} titanWeWatch 
-   */
-  function discoverTitanWEServices(titanWeWatch) {
-      console.log('Scanning for services...')
+}
+/**
+ * See if we can discover required service & chacterstic, and ssubscribe to it.
+ * @param {peripheral} titanWeWatch 
+ */
+function discoverTitanWEServices(titanWeWatch) {
+    console.log('Scanning for services...')
     titanWeWatch.discoverServices([titanWEServiceUUID], (error, services) => {
-        if(error) {
+        if (error) {
             throw error;
             return
         }
-        if(services[0]){
+        if (services[0]) {
             console.log('Services found for Titan WE watch.');
             console.log('Scanning for characteristics...');
             services[0].discoverCharacteristics(titanWECharacterstic, (error, characteristics) => {
@@ -122,47 +122,65 @@ noble.on('stateChange', function(state) {
             });
         }
     });
-  }
-
-  function buttonClickedOnTitanWEWatch(data, isNotification){
-      console.log(`Button pressed ${data}`);
-      switch(data.toString()){
-          case 'S1':
-            var switch1State = switch1.readSync();
-            switch1.writeSync(switch1State^1);
-            break;
-          case 'S2':
-            if(hueBridgeClient) {
-  hueBridgeClient.groups.getById(1)
-  .then(group => {
-    group.incrementBrightness        = 50;
-    return hueBridgeClient.groups.save(group);
-  })
-  .then(group => {
-    console.log('New brightness:', group.brightness);
-    if(group.brightness == 254){
-    hueBridgeClient.groups.getById(1)
-  .then(group => {
-    group.brightness        = 10;
-    return hueBridgeClient.groups.save(group);
-  })
-  .then(group => {
-    console.log('New brightness:', group.brightness);
-  })
-  .catch(error => {
-    console.log(error.stack);
-  }); 
 }
-  })
-  .catch(error => {
-    console.log(error.stack);
-  });
-            }
+
+function buttonClickedOnTitanWEWatch(data, isNotification) {
+    console.log(`Button pressed ${data}`);
+    switch (data.toString()) {
+        case 'S1':
+            var switch1State = switch1.readSync();
+            switch1.writeSync(switch1State ^ 1);
             break;
-          case 'S3':
+        case 'S2':
+            //changeScene();
+            findScenes();
+            break;
+        case 'S3':
             console.log('S3 clicked');
             var switch2State = switch2.readSync();
-            switch2.writeSync(switch2State^1);
-            break;  
-      }
-  }
+            switch2.writeSync(switch2State ^ 1);
+            break;
+    }
+}
+
+function changeScene() {
+    if (hueBridgeClient) {
+        hueBridgeClient.groups.getById(1)
+            .then(group => {
+                group.incrementBrightness = 50;
+                return hueBridgeClient.groups.save(group);
+            })
+            .then(group => {
+                console.log('New brightness:', group.brightness);
+                if (group.brightness == 254) {
+                    hueBridgeClient.groups.getById(1)
+                        .then(group => {
+                            group.brightness = 10;
+                            return hueBridgeClient.groups.save(group);
+                        })
+                        .then(group => {
+                            console.log('New brightness:', group.brightness);
+                        })
+                        .catch(error => {
+                            console.log(error.stack);
+                        });
+                }
+            })
+            .catch(error => {
+                console.log(error.stack);
+            });
+    }
+}
+
+function findScenes() {
+    if (hueBridgeClient) {
+        hueBridgeClient.scenes.getAll()
+            .then(scenes => {
+                for (let scene of scenes) {
+                    console.log(`Scene [${scene.id}]: ${scene.name}`);
+                    console.log('  Lights:', scene.lightIds.join(', '));
+                    console.log();
+                }
+            });
+    }
+}
