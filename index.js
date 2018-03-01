b39a917ddbd631ca29fa3f13c795803b443540d5
Pravin Raj <pravin.raj@coveiot.com>
2018-03-01 17:06:06 +0530

    Bug fixes

--- index.js
+++ index.js
@@ -66,8 +66,9 @@ var sceneNames = ['Read', 'Tropical twilight', 'Spring blossom', 'Savanna sunset
 var sceneIndex = 0;
 //Helpers variables
 var isLightsON = false;
-var THEME_CHANGE_INERVAL = 20000;
+var THEME_CHANGE_INERVAL = 10000;
 var changeTheme;
+var lightBrightness;
 
 /**
  * Initialise Ultrasonic sensor for wave detection
@@ -347,7 +348,40 @@ function handleButtonClick(data, isNotification) {
 }
 
 function toggleBrightness() {
-    console.log('Toggle brightness clicked.')
+    client.groups.getAll()
+  .then(groups => {
+    for (let group of groups) {
+      console.log(`Group [${group.id}]: ${group.name}`);
+      console.log(`  Type: ${group.type}`);
+      console.log(`  Class: ${group.class}`);
+      console.log('  Light Ids: ' + group.lightIds.join(', '));
+      console.log('  State:');
+      console.log(`    Any on:     ${group.anyOn}`);
+      console.log(`    All on:     ${group.allOn}`);
+      console.log('  Action:');
+      console.log(`    On:         ${group.on}`);
+      console.log(`    Brightness: ${group.brightness}`);
+      console.log(`    Color mode: ${group.colorMode}`);
+      console.log(`    Hue:        ${group.hue}`);
+      console.log(`    Saturation: ${group.saturation}`);
+      console.log(`    X/Y:        ${group.xy[0]}, ${group.xy[1]}`);
+      console.log(`    Color Temp: ${group.colorTemp}`);
+      console.log(`    Alert:      ${group.alert}`);
+      console.log(`    Effect:     ${group.effect}`);
+ 
+      if (group.modelId !== undefined) {
+        console.log(`  Model Id: ${group.modelId}`);
+        console.log(`  Unique Id: ${group.uniqueId}`);
+        console.log('  Model:');
+        console.log(`    Id:           ${group.model.id}`);
+        console.log(`    Manufacturer: ${group.model.manufacturer}`);
+        console.log(`    Name:         ${group.model.name}`);
+        console.log(`    Type:         ${group.model.type}`);
+      }
+ 
+      console.log();
+    }
+  });
 }
 
 function changeSceneContinuously() {
