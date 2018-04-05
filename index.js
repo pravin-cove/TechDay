2da5dfbd1503d44ea697a7e1cda4172fb0e001dc
Pravin Raj <pravin.raj@coveiot.com>
2018-04-05 18:36:39 +0530

    Aakash

--- index.js
+++ index.js
@@ -581,30 +581,6 @@ function getAllGroup() {
       console.log(`  Type: ${group.type}`);
       console.log(`  Class: ${group.class}`);
       console.log('  Light Ids: ' + group.lightIds.join(', '));
-      console.log('  State:');
-      console.log(`    Any on:     ${group.anyOn}`);
-      console.log(`    All on:     ${group.allOn}`);
-      console.log('  Action:');
-      console.log(`    On:         ${group.on}`);
-      console.log(`    Brightness: ${group.brightness}`);
-      console.log(`    Color mode: ${group.colorMode}`);
-      console.log(`    Hue:        ${group.hue}`);
-      console.log(`    Saturation: ${group.saturation}`);
-      console.log(`    X/Y:        ${group.xy[0]}, ${group.xy[1]}`);
-      console.log(`    Color Temp: ${group.colorTemp}`);
-      console.log(`    Alert:      ${group.alert}`);
-      console.log(`    Effect:     ${group.effect}`);
- 
-      if (group.modelId !== undefined) {
-        console.log(`  Model Id: ${group.modelId}`);
-        console.log(`  Unique Id: ${group.uniqueId}`);
-        console.log('  Model:');
-        console.log(`    Id:           ${group.model.id}`);
-        console.log(`    Manufacturer: ${group.model.manufacturer}`);
-        console.log(`    Name:         ${group.model.name}`);
-        console.log(`    Type:         ${group.model.type}`);
-      }
- 
       console.log();
     }
   });
